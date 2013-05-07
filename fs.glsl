b892d094ed11a724c48a4e870ea038b05d7bfeef
Devashish Tyagi <devashishrocker@gmail.com>
2013-05-07 15:43:48 +0530

    shaders updated

--- fs.glsl
+++ fs.glsl
@@ -1,6 +1,43 @@
-in vec3 colour;
+#version 420
+
+in vec3 colour, position_eye, normal_eye;
 out vec4 frag_colour;
+uniform mat4 view, proj, model, orig_model_mat;
+
+// virtual lighting source
+vec3 light_position_world = vec3(0.0, 0.0, 7.0);
+vec3 Ls = vec3(0.7, 0.7, 0.7);
+vec3 Ld = vec3(1.0, 1.0, 1.0);
+vec3 La = vec3(0.2, 0.2, 0.2);
+
+// surface reflectance
+vec3 Ks = vec3(1.0, 1.0, 1.0);
+vec3 Kd = vec3(1.0, 1.0, 0.0);
+vec3 Ka = vec3(1.0, 1.0, 1.0);
+float specular_exponent = 100.0;
+
 
 void main () {
-  frag_colour = vec4 (colour, 1.0);
+    // ambient intensity
+    vec3 la = La*Ka;
+
+    // diffuse lighting
+    vec3 light_position_eye = vec3(view * orig_model_mat * vec4(light_position_world, 1.0));
+    vec3 distance_to_light_eye = light_position_eye - position_eye;
+    vec3 direction_to_light_eye = normalize(distance_to_light_eye);
+    vec3 norm_normal_eye = normalize(normal_eye);
+    float dot_prod = max(dot(direction_to_light_eye, norm_normal_eye), 0.0);
+    vec3 ld = Ld*Kd*dot_prod; // final diffuse intensity
+    //vec3 ld = colour*dot_prod;
+
+    //specualr intensity
+    vec3 reflection_eye = reflect(-direction_to_light_eye, normal_eye);
+    reflection_eye = normalize(reflection_eye);
+    vec3 surface_to_viewer_eye = normalize(-position_eye);
+    float dot_prod_specular = max(dot(reflection_eye, surface_to_viewer_eye), 0.0);
+    float specular_factor = pow(dot_prod_specular, specular_exponent);
+    vec3 ls = Ls*Ks*specular_factor;
+
+    float t = 1.0;
+    frag_colour = vec4 ((ld)*colour, 1.0);
 }
--- vs.glsl
+++ vs.glsl
@@ -1,10 +1,15 @@
+#version 420
+
 layout(location = 0) in vec3 vertex_position;
 layout(location = 1) in vec3 vertex_colour;
+layout(location = 2) in vec3 vertex_normal;
 
-out vec3 colour;
-uniform mat4 view, proj, model;
+out vec3 colour, position_eye, normal_eye;
+uniform mat4 view, proj, model, orig_model_mat;
 
 void main () {
-  colour = vertex_colour;
-  gl_Position = proj * view * model * vec4 (vertex_position, 1.0);
+    colour = vertex_colour;
+    position_eye = vec3(view*model*vec4(vertex_position, 1.0));
+    normal_eye = vec3(view*model*vec4(vertex_normal, 0.0));
+    gl_Position = proj * view * model * vec4 (vertex_position, 1.0);
 }
